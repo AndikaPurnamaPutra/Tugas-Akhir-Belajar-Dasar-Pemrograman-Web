741cdb5148281d8178b6d8c0e08c134836c83444
AndikaPurnamaPutra <andikapratamaputra849@gmail.com>
2023-08-27 21:34:08 +0700

    Tugas Akhir - Belajar Dasar Pemrograman Web

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Navbar Mobile
+const navbarToggler = document.querySelector(".navbar-menu");
+
+navbarToggler.addEventListener("click", function () {
+    document.querySelector(".navbar-wrapper").classList.toggle("active");
+});
